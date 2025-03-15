e61ff3f8c235ba5732ede20160e56d580e10c358
becali12 <cristian.nicula@consensys.net>
2025-03-15 11:38:13 +0200

    feat: image recognition

---
+++ image_recognition.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+import base64
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv('OPENAI_KEY')
+os.environ['OPENAI_API_KEY'] = api_key
+
+client = OpenAI()
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+image_path = "hilarious_cat.jpeg"
+base64_image = encode_image(image_path)
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                { "type": "text", "text": "could this cat be considered... freaky?" },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                },
+            ],
+        }
+    ],
+)
+
+print(completion)
