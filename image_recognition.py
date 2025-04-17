bed88213ca447ac4cfe6cfd4729a6761b313150c
becali12 <cristian.nicula@consensys.net>
2025-04-17 11:51:31 +0300

    improved image recognition

--- image_recognition.py
+++ image_recognition.py
@@ -15,8 +15,22 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-image_path = "hilarious_cat.jpeg"
+image_path = "metamask-images/normal_screen.png"
 base64_image = encode_image(image_path)
+
+prompt = """This is a photo of a user's Metamask wallet. Describe what you see in the photo. 
+
+If there is an error make sure to capture it in your description. 
+
+If these details are visible, extract:
+
+ 1. the user's public address - this will not be shown if the image depicts a transaction
+ 2. the tokens that they own
+ 3. the network that the wallet has selected
+ 4. if a transaction is shown, the public address that the funds are being sent to and the details of the transaction
+
+If the image is not of the Metamask wallet, describe the error (if any) and what you see in the image.
+"""
 
 completion = client.chat.completions.create(
     model="gpt-4o",
@@ -24,7 +38,7 @@ completion = client.chat.completions.create(
         {
             "role": "user",
             "content": [
-                { "type": "text", "text": "could this cat be considered... freaky?" },
+                { "type": "text", "text": prompt },
                 {
                     "type": "image_url",
                     "image_url": {
@@ -36,4 +50,6 @@ completion = client.chat.completions.create(
     ],
 )
 
-print(completion)
+photo_description = completion.choices[0].message.content
+
+print(photo_description)
